bc35598428b53dbc4719d83d895e385f5b35c2ae
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-16 08:46:02 -0700

    update 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,11 +3,6 @@
  * main - Entry main
  *
  * Description: 'the main will print size of some various types'
- * @char: this parameter has a single character
- * @int: this parameter has a number value
- * @long int: this parameter has a more size than int
- * @long long int: this parameter has size more than int and long int
- * @float: this parameter has a biger number than int
  *
  * Return: Always 0 (success)
  */
