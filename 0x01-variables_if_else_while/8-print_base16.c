bff2f86c71815fe405bfbbf9578169f1291a0ec2
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-17 13:55:43 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry main
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 16; c++)
+{
+if (c < 10)
+	putchar (c + '0');
+else
+	putchar (c - 10 + 'a');
+}
+putchar ('\n');
+return (0);
+}
