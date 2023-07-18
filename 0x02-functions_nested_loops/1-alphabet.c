d54291b9173974513fa432c009326340674fd7eb
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-18 12:30:58 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,27 @@
+#include "main.h"
+#include<stdio.h>
 /**
  * void - print_alphabet
  *
- * Return: return()
+ * Return: Always void
  */
 void print_alphabet(void)
 {
-	for (char c = 'a'; c <= 'z'; c++)
-		putchar("%c", c);
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 	putchar('\n');
-	return ();
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
