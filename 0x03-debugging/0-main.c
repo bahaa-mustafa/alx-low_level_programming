47d745b7f685640f124b9897db7689be72462bab
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-20 08:38:42 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - check if the function will return positive or negitive
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
