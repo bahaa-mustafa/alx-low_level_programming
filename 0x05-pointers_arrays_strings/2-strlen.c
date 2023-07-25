11fe6a13d77d8e63bc518b988ca267aa25d83973
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-25 00:49:14 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * _strlen - return the length of a string
+ * @s: the string
+ *
+ * Return: number of string length
+ */
+
+int _strlen(char *s)
+{
+	int length = strlen(*s);
+
+	return (length);
+}
