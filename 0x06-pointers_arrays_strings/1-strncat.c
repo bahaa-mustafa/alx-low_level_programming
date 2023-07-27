a07fe6a7a1a89844a08c41dcacc9ebd329de5390
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-27 09:02:43 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,9 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (i = 0; dest != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 		;
-	for (j = 0; src != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		if (j < n)
 			dest[i + j] = src[j];
