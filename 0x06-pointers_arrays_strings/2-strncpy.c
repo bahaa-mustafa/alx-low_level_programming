3b154d38d1349ebf0317d10d00d75b5674089cf4
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-27 10:04:32 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,14 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (n != 0 && src[i] != '\0')
-		{
-			dest[i] = src[i];
-			n--;
-		}
+		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
