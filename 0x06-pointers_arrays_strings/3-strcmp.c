1980b766989fce301651747bf59e048bc8c0d635
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-07-27 23:38:07 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int lengthS1 = strlen(s1);
-	int lengthS2 = strlen(s2);
-
-	if (lengthS1 > lengthS2)
-		return (15);
-	else if (lengthS1 < lengthS2)
-		return (-15);
-	else
-		return (0);
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
