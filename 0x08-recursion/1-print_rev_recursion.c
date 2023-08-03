3905bbbbb162b1427f9e737b4cf68e33bf8b6da0
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-08-03 04:50:06 -0700

    1. Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,25 +8,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	if (*s != '\0' && j == 0)
-	{
-		s++;
-		i++;
-		_print_rev_recursion(s);
-	}
-
-	else
-	{
-		if (i < 0)
-			return;
-		j = 1;
-		s--;
-		i--;
-		_putchar(*s);
-		_print_rev_recursion(s);
-	}
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
