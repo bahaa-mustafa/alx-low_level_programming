e01570f6396559282d32872723fdeaed1ba03560
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-08-07 05:56:05 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x
+ * raised to the power of y
+ * @x: base number
+ * @y: number of power
+ *
+ * Return: value of x with power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (x == 1)
+		return (x);
+	if (y == 0)
+		return (-1);
+	x = x * x;
+	return (x + _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,6 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int is_palindrome(char *s);
-
+int _pow_recursion(int x, int y);
 
 #endif
