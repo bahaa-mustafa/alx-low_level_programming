2354653eb72e5f6ab69ad8b8d587c1b72397724c
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-08-08 00:04:05 -0700

    8. Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * helper - help to detect similarity in two string
+ * @s1: fiert string
+ * @s2: second string
+ *
+ * Return: 1 in similarity case, 0 if not
+ */
+int helper(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		s2++;
+		return (helper(s1, s2));
+	}
+	if (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+		return (helper(s1, s2));
+	}
+	else
+		return (0);
+}
+
+/**
+ * wildcmp - compares two strings and returns 1
+ * if the strings can be considered identical,
+ * otherwise return 0.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 in identical case, 0 in otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	return (helper(s1, s2));
+}
