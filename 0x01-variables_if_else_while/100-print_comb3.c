6bbdda5ff2ddd5c48093b30095a9421ae7f8d520
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-09-10 00:18:33 -0700

    update file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,33 @@
  */
 int main(void)
 {
-int c, a;
+int c, a, d, f;
 
 for (c = 0; c <= 9; c++)
 {
-for (a = c + 1; a <= 9; a++)
-{
-putchar (c + '0');
-putchar (a + '0');
-if (c != 8 || a != 9)
-{
-putchar (',');
-putchar (' ');
-}
-}
+	for (d = 0; d <= 9; d++)
+	{
+		for (f = 0; f <= 9; f++)
+		{
+			for (a = 0; a <= 9; a++)
+			{
+				if (d == 0 && a == 0)
+				{
+					a++;
+				}
+				putchar (c + '0');
+				putchar (d + '0');
+				putchar (' ');
+				putchar (f + '0');
+				putchar (a + '0');
+				if (c != 8 || d != 9)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
 }
 putchar ('\n');
 return (0);
