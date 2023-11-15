e1968279a68dc19b0924c9405e88006a86323305
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-11-15 12:05:45 -0800

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,22 +11,16 @@ int helper(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == '\0')
+	else if (*s1 == '\0' && *s2 != '*')
 		return (0);
-	if (*s2 == '\0')
+	else if (*s2 == '\0')
 		return (1);
-	if (*s2 == '*')
-	{
-		return (helper(s1, s2+1));
-	}
-	if (*s1 == *s2)
-	{
-		return (helper(s1+1, s2+1));
-	}
-	if (*s2 != *s1)
-	{
-		return (helper(s1+1, s2));
-	}
+	else if (*s2 == '*')
+		return (helper(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (helper(s1 + 1, s2 + 1));
+	else if (*s2 != *s1)
+		return (helper(s1 + 1, s2));
 	return (0);
 
 }
@@ -44,6 +38,6 @@ int helper(char *s1, char *s2)
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-		return (0);
+		return (1);
 	return (helper(s1, s2));
 }
