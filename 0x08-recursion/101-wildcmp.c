17d9c18389e49702657cab09eac8ee880995e994
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-11-15 12:19:46 -0800

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,9 @@ int helper(char *s1, char *s2)
 		return (helper(s1, s2 + 1));
 	else if (*s1 == *s2)
 		return (helper(s1 + 1, s2 + 1));
-	else if (*s2 != *s1)
+	else if (*s2 != *s1 && *s2 - 1 != '*')
+		return (0);
+	else if (*s2 != *s1 && *s2 - 1 == '*')
 		return (helper(s1 + 1, s2));
 	return (0);
 
