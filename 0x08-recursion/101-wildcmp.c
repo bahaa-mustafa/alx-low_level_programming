9f5b0113b84b20ccc50b9f5bc9407c466fb8facf
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-11-15 13:09:09 -0800

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,39 +1,25 @@
 #include "main.h"
 /**
- * checker - check is s2 - 1 is a * or not
- * @s2: the string
- *
- * Return: 0 if not an recursion if is true
- */
-int checker(char *s2)
-{
-	if (*s2 == '*')
-		return (helper(s1 + 1, s2));
-	return (0);
-}
-/**
  * helper - help to detect similarity in two string
  * @s1: fiert string
  * @s2: second string
+ * @f: start of string s1
+ * @n: start of string s2
  *
  * Return: 1 in similarity case, 0 if not
  */
-int helper(char *s1, char *s2)
+int helper(char *s1, char *s2, int f, int n)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+	if (s1[f] == '\0' && s2[n] == '\0')
 		return (1);
-	else if (*s1 == '\0' && *s2 != '*')
-		return (0);
-	else if (*s2 == '\0')
-		return (1);
-	else if (*s2 == '*')
-		return (helper(s1, s2 + 1));
-	else if (*s1 == *s2)
-		return (helper(s1 + 1, s2 + 1));
-	else if (*s2 != *s1)
-		return (checker(s2 - 1));
-	return (0);
+	if (s1[f] == s2[n])
+		return (s1, s2, f + 1, n + 1);
+	if (s1[f] == '\0' && s2[n] == '*')
+		return (s1, s2, f, n + 1);
+	if (s2[f] == '*')
+		return (s1, s2, f, n + 1 || s1, s2, f + 1, n);
 
+	return (0);
 }
 
 /**
@@ -48,7 +34,5 @@ int helper(char *s1, char *s2)
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	return (helper(s1, s2));
+	return (helper(s1, s2, 0, 0));
 }
