462b57c1a178456a825428913ac34fbc0f916a51
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-11-17 06:18:47 -0800

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print number of things in programe
+ * @argc: argement count in programe
+ * @argv: argement vector in a programe
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
