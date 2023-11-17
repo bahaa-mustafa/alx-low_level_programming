914366db03b47b7e63f14d48f114df76443a3d94
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-11-17 05:33:13 -0800

    first pushing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - print name of programe
+ * @argc: number of things in the programe
+ * @argv: it is an argument vector in the programe
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+this directory contain tasks for argc and argv[] project
+
+What does argc and argv mean?
+https://intranet.alxswe.com/rltoken/A0pzqslB6Z3Y3OV3hJQ6Tw
