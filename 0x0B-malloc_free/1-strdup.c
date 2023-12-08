821c61c3479820dbf6ce7d7558fd63cddc85b4b9
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-12-08 09:10:03 -0800

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,16 +15,17 @@ char *_strdup(char *str)
 {
 	char *ar;
 	int i;
-	
+
 	ar = malloc(INT_MAX);
 	i = 0;
+	if (str == NULL)
+		return (NULL);
 	while (*str != '\0')
 	{
 		ar[i] = *str;
 		str++;
 		i++;
 	}
-	if (ar == NULL)
-		return (NULL);
+	ar[i] = '\0';
 	return (ar);
 }
