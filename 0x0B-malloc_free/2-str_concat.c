9d2ff1251de319581abfeadfd39009a6a20a72e3
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-12-09 05:18:56 -0800

    solve

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -42,9 +42,9 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	for (i = 0; i <= (size1 + size2); i++)
 	{
-		if (i >= size1)
-			pt[i] = s2[i - size1];
-		pt[i] = s1[i];
+		if (i < size1)
+			pt[i] = s1[i];
+		pt[i] = s1[i - size1];
 	}
 	pt[i] = '\0';
 	return (pt);
