2967c4f757599b2af638a2c9274c0ad850791a41
Bahaa Badr <bahaamustafabadr@gmail.com>
2023-12-09 04:22:12 -0800

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,13 +11,10 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *pt = malloc(sizeof(s1) + sizeof(s2));
+	char *pt = malloc(sizeof(char *) * 2);
 	int i, j;
 
-	if (pt == 0)
-		return (NULL);
-
-	if (s1 == 0)
+	if (s1 == 0 && s2 == 0)
 		return (NULL);
 
 	i = 0;
