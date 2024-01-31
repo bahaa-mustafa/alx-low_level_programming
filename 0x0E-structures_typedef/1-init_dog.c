42ef37b1d0d309a70602ce3d03db318072134942
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-01-31 11:30:20 -0800

    task 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog init_dog;
+
+	dog->name = init_dog.nmae;
+	dog->age = init_dog.age;
+	dog->owner = init_dog.owner;
+
+}
