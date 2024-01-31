6e6d9679492ae7fcec4f0dee0dd4592d399d4902
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-01-31 12:59:32 -0800

    task 1, 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,9 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n",
-				d->name, d->age, d->owner);
-	}
-	else if (!d && !d->name)
-		printf("Name: (nil)\n");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}	
 }
