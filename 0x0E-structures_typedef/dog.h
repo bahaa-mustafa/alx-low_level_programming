f838450ce0af26f170f6ca1cd48f520c46c073f7
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-01-31 20:36:40 -0800

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - poppy description
- * @name: poppy name
- * @age: poppy age
- * @owner: poppy is owner
- */
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
+
+/**
+ *  struct dog - poppy struct as dog_t
+ *  @name: poppy name
+ *  @age: poppy age
+ *  @owner: poppy is owner
+ *
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void print_dog(struct dog *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
 #endif
