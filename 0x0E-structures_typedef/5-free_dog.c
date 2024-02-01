3b9c1eaee4cc862181a626597a6887e97e86758e
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-01-31 21:59:18 -0800

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - free stract from memory allocat
+ * @d: pointer to struct
+ * Return: no return
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
