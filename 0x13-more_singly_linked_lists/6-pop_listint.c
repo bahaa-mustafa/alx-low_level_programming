f43117b943c1eae11ad50a1bf7d8b6ecfb3ff598
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-02 07:13:41 -0800

    task 7

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delet first node and retuen his data
+ * @head: pointer to head
+ *
+ * Return: data of head node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data_n;
+	listint_t *current;
+
+	if (!head)
+		return (0);
+	current = *head;
+	data_n = current->n;
+	current = current->next;
+	*head = current;
+	return (data_n);
+}
+
