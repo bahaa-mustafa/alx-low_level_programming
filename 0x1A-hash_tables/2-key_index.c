182c6ccca06a40af9134fe904351c339363604c4
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-02-16 10:48:53 -0800

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,11 +10,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long index = hash_djb2(key);
-	hash_table_t *ht = hash_table_create(size);
-
-	if (!ht || !index)
-		return (NULL);
-
-	return (index);
+	return (hash_djb2(key) % size);
 }
