81b1e4842509c4fa82c07592aa3a78e04261da14
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-03-12 13:01:41 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,13 +22,6 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-/**
-*	if (file_from == -1)
-*	{
-*		dprintf(STDERR_FILENO, "Error: Can't open files\n");
-*		return (98);
-*	}
-*/
 	while ((char_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		char_write = write(file_to, buffer, char_read);
